dbcd3e3c4e58f3fe42598068fac318cbf90dbd60
Sarwar Nazrul <sarwar.nazrul242@gmail.com>
2025-08-08 17:06:35 -0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,12 @@ import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import "./index.css";
 import { Contact } from "./components/sections/Contact";
-import { BackgroundAnimation } from "./components/BackgroundAnimation";
-import { GradientBackground } from "./components/GradientBackground";
-
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="relative min-h-screen bg-black">
-      {/* Background animations - these stay visible */}
-      <div className="fixed inset-0">
-        <BackgroundAnimation />
-        <GradientBackground />
-      </div>
 
       {/* Loading screen */}
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
